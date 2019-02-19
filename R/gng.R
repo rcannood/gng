0525f00a69055dc9d40e43361ce264d2d613c028
Robrecht Cannoodt <rcannood@gmail.com>
2019-02-19 08:32:48 +0100

    Update gng.R

--- R/gng.R
+++ R/gng.R
@@ -256,7 +256,7 @@ gng_r <- function(x, max_iter, epsilon_b, epsilon_n, age_max, max_nodes, lambda,
 #' @importFrom igraph graph_from_edgelist layout_with_fr
 #'
 #' @export
-gng_project <- function(gng_out, make_projection = TRUE) {
+gng_project <- function(gng_out, x, make_projection = TRUE) {
   # Project the nodes to a 2D plane
   igr <- igraph::graph_from_edgelist(as.matrix(gng_out$edges), directed = FALSE)
   node_proj <- igraph::layout_with_fr(igr)
